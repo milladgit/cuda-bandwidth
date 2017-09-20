834709fbd030375df8d8d744bbebf43d5b47c2ed
Millad <millad.mg@gmail.com>
2017-09-19 20:21:35 -0500

    add extra code

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
 
 all: bw bandwidthtest
 
-bw:
+bw: bw.cu
 	nvcc -std c++11 -O3 bw.cu -o bw
 
-bandwidthtest:
+bandwidthtest: bandwidthtest.cu
 	nvcc -w -std c++11 -O3 bandwidthtest.cu -o bandwidthtest
+
+clean:
+	rm -rf bw bandwidthtest
+
--- bw.cu
+++ bw.cu
@@ -14,16 +14,8 @@ int cmpfunc (const void * a, const void * b) {
 	return ( *(double*)a - *(double*)b );
 }
 
-int main(int argc, char **argv) {
-
-	int count_per_piece = atoi(argv[1]);
-	int total_pieces = atoi(argv[2]);
-	int iterations = atoi(argv[3]);
-
+void measure_data_transfer(int count_per_piece, int total_pieces, int iterations, double *timings_h2d, double *timings_d2h) {
 	int sz = sizeof(double) * count_per_piece;
-
-	double *timings_h2d = (double*) malloc(sizeof(double) * iterations);
-	double *timings_d2h = (double*) malloc(sizeof(double) * iterations);
 
 	struct timespec t1, t2;
 	double diff;
@@ -61,7 +53,7 @@ int main(int argc, char **argv) {
 		}
 		if(clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t2) != 0) {fprintf(stderr, "Error in timer.\n"); exit(1);}
 		diff = (t2.tv_sec - t1.tv_sec) * 1E9 + (t2.tv_nsec - t1.tv_nsec);
-		timings_h2d[q] = diff;
+		timings_h2d[q] = diff / total_pieces;
 
 
 
@@ -73,7 +65,7 @@ int main(int argc, char **argv) {
 		}
 		if(clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t2) != 0) {fprintf(stderr, "Error in timer.\n"); exit(1);}
 		diff = (t2.tv_sec - t1.tv_sec) * 1E9 + (t2.tv_nsec - t1.tv_nsec);
-		timings_d2h[q] = diff;
+		timings_d2h[q] = diff / total_pieces;
 
 
 
@@ -85,13 +77,81 @@ int main(int argc, char **argv) {
 
 		free(data_host);
 		free(data_device);
+	}	
+}
+
+int main(int argc, char **argv) {
+
+	if(argc < 3) {
+		fprintf(stderr, "Usage: %s <count_per_piece> <iterations>\n", argv[0]);
+		return 1;
 	}
+
+	int count_per_piece = atoi(argv[1]);
+	int iterations = atoi(argv[2]);
+	int total_pieces;
+
+	if(iterations < 2) {
+		fprintf(stderr, "Unacceptable value for <iterations> parameter.\n");
+		return 1;
+	}
+
+	double *timings_h2d = (double*) malloc(sizeof(double) * iterations);
+	double *timings_d2h = (double*) malloc(sizeof(double) * iterations);
+
+	fprintf(stderr, "Warmup...\n");
+	measure_data_transfer(count_per_piece, 10, 1, &timings_h2d[0], &timings_d2h[0]);
+	fprintf(stderr, "Warmup...Done\n");
+	double t1 = 0, t2 = 0;
+	for(int i=0;i<10;i++) { 
+		t1 += timings_h2d[i];
+		t2 += timings_d2h[i];
+	}
+	if(t2 < t1) 
+		t1 = t2;
+
+	t1 *= 1E-9;
+
+	printf("t: %.10f\n", t1);
+
+	int max_total_pieces = 500000;
+	total_pieces = (int) ceil(1.0 / t1);
+	if(total_pieces > max_total_pieces) {
+		fprintf(stderr, "Number of iterations has exceeded more than %d (it was %d)\n", max_total_pieces, total_pieces);
+		total_pieces = max_total_pieces;
+	}
+	fprintf(stderr, "Number of iterations to reach at least 1 second: %d\n", total_pieces);
+
+
+	fprintf(stderr, "Main operations...\n");
+	measure_data_transfer(count_per_piece, total_pieces, iterations, &timings_h2d[0], &timings_d2h[0]);
+	fprintf(stderr, "Main operations...Done\n");
+
 
 	qsort(timings_h2d, iterations, sizeof(double), cmpfunc);
 	qsort(timings_d2h, iterations, sizeof(double), cmpfunc);
 
-	printf("H2D median: %.2fms\n", timings_h2d[iterations/2] / 1E6);
-	printf("D2H median: %.2fms\n", timings_d2h[iterations/2] / 1E6);
+	double t_h2d, t_d2h;
+	if(iterations % 2 == 1) {
+		t_h2d = timings_h2d[iterations/2];
+		t_d2h = timings_d2h[iterations/2];
+	} else {
+		t_h2d = timings_h2d[iterations/2];
+		t_h2d += timings_h2d[iterations/2 - 1];
+		t_h2d /= 2;
+
+		t_d2h = timings_d2h[iterations/2];
+		t_d2h += timings_d2h[iterations/2 - 1];
+		t_d2h /= 2;
+	}
+
+	printf("H2D median: %.2fus\n", t_h2d / 1E3);
+	printf("D2H median: %.2fus\n", t_d2h / 1E3);
+
+	printf("------------------------\n");
+
+	printf("H2D median: %.2fms\n", t_h2d / 1E6);
+	printf("D2H median: %.2fms\n", t_d2h / 1E6);
 
 	return 0;
 }
